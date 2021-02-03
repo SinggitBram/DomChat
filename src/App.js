f21ced6964473d71dd2df485fb96fcb49104723e
SinggitBram <bambadom@gmail.com>
2021-02-04 03:13:42 +0700

    adding login screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
 const App = () => {
+    if (!localStorage.getItem("username")) return <LoginForm />;
+
     return (
         <ChatEngine
             height="100vh"
             projectID="8e4dad52-9195-4be0-9798-a7b119a0d1db"
-            userName="singgitbram"
-            userSecret="123123"
+            userName={localStorage.getItem("username")}
+            userSecret={localStorage.getItem("password")}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} //supaya yg dipake component kita
         />
     );
--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,10 +1,24 @@
-import {useState} from 'react'
+import { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
 
-const MessageForm = () => {
-    const [value, setValue] = useState('')
+const MessageForm = (props) => {
+    const [value, setValue] = useState("");
+    const { chatId, creds } = props;
 
-    const handleSubmit = () => {};
-    const handleChange = () => {};
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const text = value.trim();
+        if (text.length > 0) sendMessage(creds, chatId, { text });
+        setValue("");
+    };
+    const handleChange = (event) => {
+        setValue(event.target.value);
+        isTyping(props, chatId);
+    };
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.files, text: "" });
+    };
 
     return (
         <form className="message-form" onSubmit={handleSubmit}>
@@ -14,7 +28,22 @@ const MessageForm = () => {
                 placeholder="Send a message"
                 value={value}
                 onChange={handleChange}
+                onSubmit={handleSubmit}
             />
+            <label htmlFor="upload-button">
+                <span className="image-button"></span>
+                <PictureOutlined className="picture-icon" />
+            </label>
+            <input
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: "none" }}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
         </form>
     );
 };
